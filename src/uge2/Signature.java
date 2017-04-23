86d93ca9efd447338ee2bf8959c66e7f80530cc5
Christoffer Hauthorn <christoffer@emplate.it>
2017-04-23 13:16:22 +0200

    Signature without hashing implemented

--- src/uge2/Signature.java
+++ src/uge2/Signature.java
@@ -1,6 +1,7 @@
 package uge2;
 
 import uge1.Modulo;
+import uge1.RSA;
 
 import java.math.BigInteger;
 
@@ -8,12 +9,12 @@ import java.math.BigInteger;
  * Hashes and applies a signature to a message
  */
 public class Signature {
-    public static BigInteger createSignature(Modulo modulo) {
-        return BigInteger.ONE;
+    public static BigInteger createSignature(BigInteger message, Modulo modulo) {
+        return RSA.decrypt(message, modulo);
     }
 
     public static boolean verifySignature(BigInteger message, BigInteger signature, Modulo modulo) {
-        return false;
+        return RSA.encrypt(signature, modulo).equals(message);
     }
 }
 
--- test/uge2/SignatureTest.java
+++ test/uge2/SignatureTest.java
@@ -1,6 +1,10 @@
 package uge2;
 
 import org.junit.Test;
+import uge1.Modulo;
+import uge1.RSA;
+
+import java.math.BigInteger;
 
 import static org.junit.Assert.*;
 
@@ -9,12 +13,16 @@ import static org.junit.Assert.*;
  */
 public class SignatureTest {
     @Test
-    public void canCreateSignature() throws Exception {
-
-    }
-
-    @Test
     public void canVerifySignature() throws Exception {
-        
+        BigInteger message = new BigInteger("42");
+        Modulo modulo = RSA.keyGen(2000);
+
+        BigInteger signature = Signature.createSignature(message, modulo);
+
+        assertTrue(Signature.verifySignature(message, signature, modulo));
+
+        // Modify the message, expect verification to fail
+        message = message.add(BigInteger.ONE);
+        assertFalse(Signature.verifySignature(message, signature, modulo));
     }
 }
