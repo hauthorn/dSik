d36236f552f5ff3d92e5a1f7f07cf1c2285a505a
Christoffer Hauthorn <christoffer@emplate.it>
2017-04-23 14:06:43 +0200

    Hashing implmented for signatures, passing tests

--- src/uge2/Signature.java
+++ src/uge2/Signature.java
@@ -4,17 +4,31 @@ import uge1.Modulo;
 import uge1.RSA;
 
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
- * Hashes and applies a signature to a message
+ * Hashes and applies a signature to a message.
+ *
+ * Uses the {@link RSA} class.
  */
 public class Signature {
     public static BigInteger createSignature(BigInteger message, Modulo modulo) {
-        return RSA.decrypt(message, modulo);
+        return RSA.decrypt(digestAsBigInt(message), modulo);
     }
 
     public static boolean verifySignature(BigInteger message, BigInteger signature, Modulo modulo) {
-        return RSA.encrypt(signature, modulo).equals(message);
+        return RSA.encrypt(signature, modulo).equals(digestAsBigInt(message));
+    }
+
+    private static BigInteger digestAsBigInt(BigInteger message) {
+        try {
+            MessageDigest instance = MessageDigest.getInstance("SHA-256");
+            return new BigInteger(1, instance.digest(message.toByteArray()));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Digest went wrong.");
     }
 }
 
